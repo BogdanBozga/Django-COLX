329ebdef18d1c90c49a431134fa6edd8e1c9285e
BogdanBozga <bogdan.bozga01@gmail.com>
2025-11-30 16:28:20 +0200

    Rename from OLXCopy to COLX

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OLXClone.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'COLX.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

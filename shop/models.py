8cd020d313269f0131af91bab943908f6f04722b
BogdanBozga <bogdan.bozga01@gmail.com>
2025-12-14 18:04:44 +0200

    Up to T4

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,8 @@
+import datetime
+
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
 
 class UserProfile(models.Model):
@@ -23,6 +26,8 @@ class Product(models.Model):
     images = models.ImageField(upload_to='products/', blank=True)
     def __str__(self):
         return self.title
+    def is_recent_product(self):
+        return self.added_date >= timezone.now() - datetime.timedelta(days=1)
 
 def product_image_path(instance, filename):
     # Images go to products/<product_id>/<filename>
--- shop/urls.py
+++ shop/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'shop'
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:product_id>/", views.product_detail, name="product"),
-    path("<int:user_id>/", views.user_detail, name="user"),
+    path("home/<int:product_id>/", views.product_detail, name="product"),
+    path("user/<int:user_id>/", views.user_detail, name="user"),
     path("home/", views.home, name="home")
 ]

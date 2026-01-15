928f10e6fa9dfb98d878398470ea8a835adc4524
BogdanBozga <bogdan.bozga01@gmail.com>
2026-01-15 07:58:30 +0200

    Trying to add logo

--- shop/urls.py
+++ shop/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'shop'
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("",  views.home, name="index"),
     path("home/<int:product_id>/", views.product_detail, name="product"),
     path("user/<int:user_id>/", views.user_detail, name="user"),
     path("home/", views.home, name="home")
